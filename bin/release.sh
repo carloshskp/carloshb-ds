6d97636b8a03d3b80059ec293c2f726251265599
Carlos Bernardes <contato@carloshb.com.br>
2026-01-22 00:41:52 -0300

    feat(release): add release script and change scripts

---
+++ bin/release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get the current version from package.json
+VERSION=$(cat package.json | jq -r '.version')
+
+# Add the package.json to the commit
+git add package.json
+
+# Commit the change
+git commit -m "Bump version to $VERSION"
+
+# Tag the release
+git tag -a "v$VERSION" -m "Bump version to $VERSION"
+
+# Push the release
+# git push --tags
